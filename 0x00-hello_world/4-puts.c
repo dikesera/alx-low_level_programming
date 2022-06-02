b0a0c401489e3927a47241aa52f4fd4fb121782e
dikesera <seraphinapatricks@gmail.com>
2022-06-02 15:51:04 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry Point
+
+ *
+
+ * Description: output "Programming is like building a multilingual puzzle"
+
+ *
+
+ * Return: Always 0(success)
+
+ */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
