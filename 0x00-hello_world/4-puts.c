2d8339b1efef0d674a23f6c8ccd7769ba54b2ef1
dikesera <seraphinapatricks@gmail.com>
2022-06-02 15:56:22 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,25 +3,16 @@
 
 
 /**
-
  * main - Entry Point
-
  *
-
  * Description: output "Programming is like building a multilingual puzzle"
-
  *
-
  * Return: Always 0(success)
-
  */
-
-
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
