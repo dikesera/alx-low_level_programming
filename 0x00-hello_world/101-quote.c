444f2a8d5d014674a065a6815b1e42d07a185eb8
dikesera <seraphinapatricks@gmail.com>
2022-06-02 17:19:14 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,9 @@
  *Return: Always 1 (Success)
  *
  */
-
 int main(void)
 {
-char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-write(STDOUT_FILENO,  err, 59);
-return (1);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDOUT_FILENO,  err, 59);
+	return (1);
 }
