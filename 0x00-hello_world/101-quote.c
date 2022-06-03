716de1797d1aef9e4e92178b14a3a3d9bf8844d2
dikesera <seraphinapatricks@gmail.com>
2022-06-02 17:26:08 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
  */
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO,  err, 59);
+char don[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, don, 59);
 	return (1);
 }
