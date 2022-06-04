68a4781ea0a3efee4e12e9529d99b6d49c1946ed
dikesera <seraphinapatricks@gmail.com>
2022-06-03 17:29:34 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,47 +1,20 @@
-es (17 sloc)  309 Bytes
-
-   
-
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry Point
  *
- *  * main - Entry Point
+ * Description: prints the alphabet in lowercase, omits q and e
  *
- *   *
- *
- *    * Description: prints the alphabet in lowercase, followed by a new line
- *
- *     *
- *
- *      * Return: Always 0(Success)
- *
- *       */
-
-
-
-
+ *  Return: Always 0(Success)
+ */
 
 int main(void)
-
 {
+	char alpha[] = "abcdfghijklmnoprstuvwxyz";
+	int i;
 
-		char alpha[] = "abcdfghijklmnoprstuvwxyz";
-
-
-
-			int i;
-
-
-
-				for (i = 0; i < 24; i++)
-
-							putchar(alpha[i]);
-
-					putchar('\n');
-
-						return (0);
-
+	for (i = 0; i < 24; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
 }
