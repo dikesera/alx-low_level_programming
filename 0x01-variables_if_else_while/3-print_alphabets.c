2073c408b62490663e8493a0db642db14d2eef20
dikesera <seraphinapatricks@gmail.com>
2022-06-03 17:23:47 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,43 +1,20 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry Point
  *
- *  * main - Entry Point
+ * Description: print the alphabets in both cases using the putchar func
  *
- *   *
- *
- *    * Description: print the alphabets in both cases using the putchar function
- *
- *     *
- *
- *      * Return: Always 0(Success)
- *
- *       */
-
-
-
-
+ * Return: Always 0(Success)
+ */
 
 int main(void)
-
 {
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-		char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-
-
-			int i;
-
-
-
-				for (i = 0; i < 52; i++)
-
-							putchar(alpha[i]);
-
-					putchar('\n');
-
-						return (0);
-
+	for (i = 0; i < 52; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
 }
