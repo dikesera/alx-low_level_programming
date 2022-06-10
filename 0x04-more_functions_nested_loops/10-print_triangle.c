d0a19999a422b61d08d087d3e61961eef91515b5
dikesera <seraphinapatricks@gmail.com>
2022-06-09 19:29:52 -0700

    late to assignment

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: int
+ *
+ * Return: Nothing
+ *
+ */
+
+ void print_triangle(int size)
+ {
+ 	 int i, j,  s;
+
+	 if (size > 0)
+	 {
+		 for (i = 1; i <= size; i++)
+		 {
+			 for (s = size - i; s > 0; s--)
+				 _putchar(' ');
+		 	 for (j = 0; j < i; j++)
+				 _putchar('#');
+			 if (i == size)
+			 	 break;
+			 _putchar('\n');
+	 	 }
+																															}
+
+	 _putchar('\n');
+ }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integer
+ *
+ * @a: int
+ *
+ * @b: int
+ *
+ * Return: int
+ *
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
