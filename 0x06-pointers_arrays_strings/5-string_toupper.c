4cbbfae8a10fd2b77ee5b4f9d4633935fd9c5af4
dikesera <seraphinapatricks@gmail.com>
2022-06-16 13:55:20 -0700

    more on pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert string to all uppercase
+ *
+ * @str: char pointer
+ *
+ * Return: char pointer
+ */
+
+char *string_toupper(char *str)
+{
+	char l;
+
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			if (str[i] == l)
+			{
+				int num = ((int)str[i]) - 32;
+
+				str[i] = (char)num;
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}
