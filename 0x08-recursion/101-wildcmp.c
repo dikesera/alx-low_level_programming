fd07a9775e1473925e3adf95ae15964184a0a623
dikesera <seraphinapatricks@gmail.com>
2022-06-22 19:41:52 -0700

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,15 +5,16 @@
  * @s2: Second input string
  * Return: 1 if strings identical and 0 in otherwise
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1 && !*s2)
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) && *s2)
-		return (0);
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+
 	return (0);
 }
